2b66f3b3526e1abb3338c9c565718e44b1bba25b
Lucas <lucaaspuhl@gmail.com>
2022-11-19 10:00:04 -0800

    app ready

--- api.py
+++ api.py
@@ -27,6 +27,14 @@ books = [
 def read_all() -> jsonify:
     return jsonify(books)
 
+
+@app.route('/livros/add/', methods=['GET'])
+def create() -> None:
+    new_book = request.get_json()
+    books.append(new_book)
+    return jsonify(new_book)
+
+
 @app.route('/livros/search/<int:id>', methods=['GET'])
 def read_id(id) -> jsonify:
     for items in books:
@@ -35,23 +43,21 @@ def read_id(id) -> jsonify:
     return jsonify(books)
 
 
-@app.route('/livros/delete/<int:id>', methods=['POST', 'GET'])
+@app.route('/livros/edit/<id>', methods=['PUT'])
+def update(id) -> jsonify:
+    book_modified = request.get_json()
+    for index, item in enumerate(books):
+        if item.get('id') == id:
+            item.update(book_modified)
+    return jsonify(books)
+
+
+@app.route('/livros/delete/<id>', methods=['POST'])
 def delete_id(id) -> None:
     for index, item in enumerate(books):
         if item.get('id') == id:
             del books[index]
     return jsonify(books)
-
-
-@app.route('/livros/edit/<int:id>', methods=['PUT', 'POST', 'GET'])
-def update(id) -> jsonify:
-    #book_modified = request.get_json()
-    new_data = {'id': 3, 'title': 'None', 'Author': 'Hábitos Atômicos'}
-    for index, item in enumerate(books):
-        if item.get('id') == id:
-            item.update(new_data)
-    return jsonify(books)
-
 
 
 app.run(port=5000, host='localhost', debug=True)
